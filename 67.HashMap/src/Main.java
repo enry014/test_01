0e1476b2625a6c1831ff4c9e12a1407b58eb7c8d
Ivan Puljic <ivan.puljic@ericsson.com>
2025-12-15 18:06:43 +0100

    67.HashMap

---
+++ 67.HashMap/src/Main.java
@@ -0,0 +1,38 @@
+import java.nio.MappedByteBuffer;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args){
+
+        // HashMap = A data structure that stores key-value pairs
+        //           Keys are unique, but Values can be duplicated
+        //           Does not maintain any order, but is memory efficient
+        //           HashMap<Key, Value>
+
+        HashMap<String, Double> map = new HashMap<>();
+
+        map.put("apple", 0.50);
+        map.put("orange", 0.75);
+        map.put("banana", 0.25);
+        map.put("coconut", 1.00);
+
+        //map.remove("apple");
+        System.out.println(map);
+
+        System.out.println(map.get("apple"));
+
+        System.out.println(map.containsKey("banana"));
+
+        System.out.println(map.containsValue(1.00));
+
+        System.out.println(map.size());
+
+        for(String key : map.keySet()){
+            System.out.println(key + " : $" + map.get(key));
+        }
+
+
+
+
+    }
+}
